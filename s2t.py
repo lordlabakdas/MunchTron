9c254064534141f13c09b7de713230a3b4697fe0
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2017-02-10 07:35:32 -0600

    added mouth and ears functionalities

--- s2t.py
+++ s2t.py
@@ -4,22 +4,38 @@
 import speech_recognition as sr
 import ConfigParser
 import json
+import pyttsx
 
-cf = ConfigParser.ConfigParser()
-cf.read('/home/lordlabakdas/Desktop/config.py')
-api_key=cf.get('speech', 'API_KEY')
-print api_key
+
+def ears():
 # obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-with open ('Speech.json') as json_file:
-    json_key = json.load(json_file)
-# recognize speech using Google Speech Recognition
-try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google_cloud(audio, credentials_json=json.dumps(json_key)))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    cf = ConfigParser.ConfigParser()
+    cf.read('/home/lordlabakdas/Desktop/config.py')
+    api_key = cf.get('speech', 'API_KEY')
+    print api_key
+    with open('Speech.json') as json_file:
+        json_key = json.load(json_file)
+        r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    # recognize speech using Google Speech Recognition
+    try:
+            return r.recognize_google_cloud(audio, credentials_json=json.dumps(json_key))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print(
+            "Could not request results from Google Speech Recognition service; {0}".format(e))
+    return ""
+
+def mouth(text):
+    speech_engine = pyttsx.init('espeak') # see http://pyttsx.readthedocs.org/en/latest/engine.html#pyttsx.init
+    speech_engine.setProperty('rate', 150)
+    speech_engine.say(text)
+    speech_engine.runAndWait()
+
+mouth("Say Something")
+mouth("I heard you say " + ears())

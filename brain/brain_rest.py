0998d09ceb6f1d3722ad58bd220e7339d418e477
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2017-03-20 15:54:50 -0500

    post rest api working

--- brain/brain_rest.py
+++ brain/brain_rest.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 from brain import brain
 
 app = Flask(__name__)
-
+br = brain()
 @app.route("/weather")
 def weather():
-    br = brain()
+
     w_stats = br.get_weather()
     return w_stats
+
+@app.route("/twitter", methods = ['GET', 'POST'])
+def twitter():
+    if request.method == "POST":
+        data = request.get_json()
+        print data['sentence']
+        return br.twitter(data['sentence'])[0]
 
 
 if __name__ == "__main__":
--- brain/sentence_parser.py
+++ brain/sentence_parser.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class SentenceParser():
     def parse_sentence(self, words):
@@ -10,12 +11,18 @@ class SentenceParser():
             result = requests.get("http://localhost:5000/weather").text
             print result
             return result
+
         elif 'twitter'  in words or 'tweet' in words:
             print "twitter"
-            return self.twitter(words)
+            words = {"sentence":words}
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            result = requests.post ("http://localhost:5000/twitter", data=json.dumps(words), headers=headers)
+            return result.text
+
         elif 'restaurants' in words or 'food' in words:
             print "yelp"
             return self.yelp(words)
+
         elif "play" in words:
             print "spotify"
             return self.spotify(words)

bcbaae32da4dd062d02d1020cb89bdb2e6457438
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2017-03-25 14:42:12 -0500

    rest actions complete

--- brain/brain_rest.py
+++ brain/brain_rest.py
@@ -16,6 +16,19 @@ def twitter():
         print data['sentence']
         return br.twitter(data['sentence'])[0]
 
+@app.route("/yelp", methods = ['GET','POST'])
+def yelp():
+    if request.method == 'POST':
+        data = request.get_json()
+        print data['sentence']
+        return br.yelp(data['sentence'])
+
+@app.route("/spotify", methods = ['GET', 'POST'])
+def spotify():
+    if request.method == 'POST':
+        data = request.get_json()
+        print data['sentence']
+        return br.spotify(data['sentence'])
 
 if __name__ == "__main__":
     app.run()
--- brain/sentence_parser.py
+++ brain/sentence_parser.py
@@ -5,6 +5,8 @@ class SentenceParser():
     def parse_sentence(self, words):
         print words + "parse_sentence"
         #self.sentiment_analysis(words)
+        words = {"sentence":words}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         if 'weather' in words:
             print "weather"
             #return self.get_weather()
@@ -14,18 +16,19 @@ class SentenceParser():
 
         elif 'twitter'  in words or 'tweet' in words:
             print "twitter"
-            words = {"sentence":words}
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
             result = requests.post ("http://localhost:5000/twitter", data=json.dumps(words), headers=headers)
             return result.text
 
         elif 'restaurants' in words or 'food' in words:
             print "yelp"
-            return self.yelp(words)
+            result = requests.post ("http://localhost:5000/yelp", data=json.dumps(words), headers=headers)
+            return result.text
+
 
         elif "play" in words:
             print "spotify"
-            return self.spotify(words)
+            result = requests.post ("http://localhost:5000/spotify", data=json.dumps(words), headers=headers)
+            return result.text
         else:
             print None
             return None
